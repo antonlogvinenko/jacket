85f5e3be9c39b0fff64f30f7c5df4bcf4165e037
Anton Logvinenko <logvinenko@farpost.com>
2013-05-01 14:35:01 +1100

    master Leiningen project structure

---
+++ src/small_jvm_lisp/core.clj
@@ -0,0 +1,1 @@
+(ns small-jvm-lisp.core)
---
+++ test/small_jvm_lisp/test/core.clj
@@ -0,0 +1,6 @@
+(ns small-jvm-lisp.test.core
+  (:use [small-jvm-lisp.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
