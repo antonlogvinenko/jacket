d8227138a7f2ec12e58e14c4b1af865aa5f0a399
Anton Logvinenko <logvinenko@farpost.com>
2013-05-03 15:24:56 +1100

    master First lexer draft

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject small-jvm-lisp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.reader "0.7.4"]
+                 ]
   :main small-jvm-lisp.core)
--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -1,4 +1,35 @@
-(ns small-jvm-lisp.lexer)
+(ns small-jvm-lisp.lexer
+  (:require [clojure.tools.reader.reader-types :as rt]))
+
+(def LB :LB)
+(def RB :RB)
+
+(defn is-letter [ch]
+  (if (nil? ch) false (Character/isLetter ch)))
+
+(defn read-word [reader]
+  (let [word (StringBuffer.)]
+    (while (is-letter (rt/peek-char reader))
+      (.append word (rt/read-char reader)))
+    word))
+
+(defn read-token [reader]
+  (loop [reader reader]
+    (let [ch (rt/peek-char reader)]
+      (condp = ch
+        \( (do (rt/read-char reader) LB)
+        \) (do (rt/read-char reader) RB)
+        \space (do (rt/read-char reader)
+                (recur reader))
+        nil nil
+        (read-word reader)))))
 
 (defn tokenize [text]
-  [])
+  (loop [tokens [] reader (rt/string-push-back-reader text)]
+    (let [token (read-token reader)]
+      (if (nil? token)
+        tokens
+        (recur (conj tokens token) reader)))))
+      
+    
+    
