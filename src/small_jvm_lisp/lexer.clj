cf112c03294d4fb29c106d5ebb1fefc2147dae70
Anton Logvinenko <logvinenko@farpost.com>
2013-05-04 23:29:44 +1100

    master Lexing more

--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -4,9 +4,14 @@
 (def LB :LB)
 (def RB :RB)
 
-;;constants: string, integer, float, #t/#f
-;;keywords - def lambda + - / * . readln println ' cons car cdr fn
+
+;;read-string-constant
+;;read-number-constant
+;;read and validate keywords - def lambda + - / * . readln println ' cons car cdr fn
+;;test all read-literal cases and subfunctions
+
 ;;exceptions on unknown characters
+;;exceptions at all
 
 ;;better tokens info - map or data type?
 ;;lines numbers - change parser to read line by line
@@ -14,11 +19,35 @@
 (defn is-letter [ch]
   (if (nil? ch) false (Character/isLetter ch)))
 
-(defn read-literal [reader]
+;;make read-word from read-literal
+;;parsing #t #f numbers strings, then word if created, then exception
+(defn read-keyword [reader]
   (let [word (StringBuffer.)]
     (while (->> reader rt/peek-char is-letter)
       (->> reader rt/read-char (.append word)))
     (.toString word)))
+
+(defn read-boolean-constant [reader]
+  (let [first-char (rt/read-char reader)
+        second-char (rt/read-char reader)]
+    (if (= \# first-char)
+      (case second-char
+        \t :true
+        \f :false
+        :else (throw (RuntimeException. "Oops")))
+      (throw (RuntimeException. "Ooops")))))
+        
+(defn read-string-constant [reader])
+(defn read-number-constant [reader])
+
+(defn read-literal [reader]
+  (let [first-char (rt/peek-char reader)
+        method (condp matches first-char
+                 \# read-boolean-constant
+                 \" read-string-constant
+                 [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9] read-number-constant
+                 read-keyword)]
+    (method reader)))
 
 (defn matches [coll key]
   (if (coll? coll)
