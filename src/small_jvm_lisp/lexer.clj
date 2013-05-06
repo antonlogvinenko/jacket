109173a48e104f89c0c378baa9a5fcabc0b89a46
Anton Logvinenko <logvinenko@farpost.com>
2013-05-06 22:06:06 +1100

    master Fixed state machine

--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -122,9 +122,9 @@
 ;; :start starts with ""
 ;; going to :start with something - put it, nil - don't put
 ;; state transition - pass next, nothing - append character
-(defn append [sb char]
+(defn append [sb reader char]
   (.append sb char))
-(defn skip-char [sb char]
+(defn skip-char [sb reader char]
   sb)
 (defn parse-integer [sb]
   (-> sb .toString Integer/parseInt))
@@ -132,49 +132,46 @@
   (-> sb .toString Double/parseDouble))
 (defn keywordize [sb]
   (-> sb .toString keyword))
-(defn skip-all [sb f] nil)
-(def grammar {
-              :start
-              {\( [:start LB]
-               \) [:start RB]
-               \" [:string skip-char]
-               whitespace? [:start skip-all]
-               \# [:boolean skip-char]
-               digit? [:integer append]
-               letter? [:name append]
-               }
-              
-              :string
-              {
-               letter? [:string append]
-               \" [:start skip-char #(.toString %)]
-               }
-              
-              :boolean
-              {\t [:start true]
-               \f [:start false]
-               }
-
-              :integer
-              {
-               digit? [:integer append]
-               \. [:double append]
-               (comp not digit?) [:start skip-char parse-integer]
-               }
-
-              :double
-              {
-               digit? [:double append]
-               (comp not digit?) [:start skip-char parse-double]
-               }
-
-              :name
-              {
-               letter? [:name append]
-               whitespace? [:start skip-char keywordize]
-               }
+(defn skip-all [sb reader f] nil)
+(defn return-char [sb reader char]
+  (rt/unread reader char)
+  sb)
+;;  (rt/unread reader char))
+;; :smth -> :done always returns character
+(def grammar {:done {
+                      \( [:done LB]
+                      \) [:done RB]
+                      \" [:string skip-char]
+                      whitespace? [:done skip-all]
+                      \# [:boolean skip-char]
+                      digit? [:integer append]
+                      word-symbol? [:name append]
+                      }
+              :string {
+                       letter? [:string append]
+                       \" [:done skip-char #(.toString %)]
+                       }
+              :boolean {
+                        \t [:boolean true]
+                        \f [:boolean false]
+                        whitespace? [:done return-char]
+                        \) [:done return-char]
+                        \( [:done return-char]
+                        }
+              :integer {
+                        digit? [:integer append]
+                        \. [:double append]
+                        (comp not digit?) [:done return-char parse-integer]
+                        }
+              :double {
+                       digit? [:double append]
+                       whitespace? [:done return-char parse-double]
+                       }
+              :name {
+                     word-symbol? [:name append]
+                     (comp not word-symbol?) [:done return-char keywordize]
+                     }
               })
-
 
 (defn find-action [directions ch]
   (directions (->> directions
@@ -183,24 +180,26 @@
                    first
                    )))
   
-(defn modify [accum ch modifications]
+(defn modify [accum reader ch modifications]
   (if (-> modifications first fn? not)
     (first modifications)
-    ((apply comp (reverse modifications)) accum ch)))
-  
+    ((apply comp (reverse modifications)) accum reader ch)))
+
+(defn tokenize [string]
+  (tokenize-with-grammar grammar string))
+
 (defn tokenize-with-grammar [grammar string]
   (let [reader (rt/string-push-back-reader string)]
-    (loop [state :start accum nil reader reader tokens []]
-      (let [tokens (if (and (= state :start) (-> accum nil? not))
+    (loop [state :done accum nil reader reader tokens []]
+      (let [tokens (if (and (= state :done) (-> accum nil? not))
                      (conj tokens accum) tokens)
-            accum (if (= state :start) (StringBuffer.) accum)
+            accum (if (= state :done) (StringBuffer.) accum)
             ch (rt/read-char reader)]
-        (if (and (= state :start) (nil? ch))
+        (if (and (= state :done) (nil? ch))
           tokens
-          (let [directions (state grammar)
-                direction (find-action directions ch)
+          (let [direction (-> grammar state (find-action ch))
                 next-state (first direction)
-                next-accum (modify accum ch (rest direction))]
+                next-accum (modify accum reader ch (rest direction))]
             (recur next-state next-accum reader tokens)))))))
       
       
