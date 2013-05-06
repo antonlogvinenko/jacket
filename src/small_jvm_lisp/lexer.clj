727dc2de1e3083c6bcca3bd2c58dcde92be498da
Anton Logvinenko <logvinenko@farpost.com>
2013-05-06 22:53:20 +1100

    master Fixes

--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -94,31 +94,32 @@
                      (comp not word-symbol?) [:done return-char keywordize]}
               })
 
-(defn find-action [directions ch]
-  (->> directions
+(defn find-transition [transitions ch]
+  (->> transitions
        keys
        (filter #(or (= ch %) (and (fn? %) (% ch))))
        first
-       directions))
+       transitions))
   
 (defn modify [accum reader ch modifications]
   (if (-> modifications first fn? not)
     (first modifications)
     ((apply comp (reverse modifications)) accum reader ch)))
 
-(defn tokenize-with-grammar [grammar string]
-  (let [reader (rt/string-push-back-reader string)]
-    (loop [state :done accum nil reader reader tokens []]
-      (let [tokens (if (and (= state :done) (-> accum nil? not))
-                     (conj tokens accum) tokens)
-            accum (if (= state :done) (StringBuffer.) accum)
-            ch (rt/read-char reader)]
-        (if (and (= state :done) (nil? ch))
-          tokens
-          (let [direction (-> grammar state (find-action ch))
-                next-state (first direction)
-                next-accum (modify accum reader ch (rest direction))]
-            (recur next-state next-accum reader tokens)))))))
+(defn tokenize-with-grammar [grammar reader]
+  (loop [state :done accum nil reader reader tokens []]
+    (let [tokens (if (and (= state :done) (-> accum nil? not))
+                   (conj tokens accum) tokens)
+          accum (if (= state :done) (StringBuffer.) accum)
+          ch (rt/read-char reader)]
+      (if (and (= state :done) (nil? ch))
+        tokens
+        (let [transition (-> grammar state (find-transition ch))
+              next-state (first transition)
+              next-accum (modify accum reader ch (rest transition))]
+          (recur next-state next-accum reader tokens))))))
 
 (defn tokenize [string]
-  (tokenize-with-grammar grammar string))
+  (->> string
+       rt/string-push-back-reader
+       (tokenize-with-grammar grammar)))
