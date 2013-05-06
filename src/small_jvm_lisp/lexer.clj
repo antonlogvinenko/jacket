ee40ea214cfa2ad88d65d12b42960f6436dded2a
Anton Logvinenko <logvinenko@farpost.com>
2013-05-06 16:20:24 +1100

    master Fuck, implemented a state machine for a grammar

--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -1,8 +1,6 @@
 (ns small-jvm-lisp.lexer
   (:require [clojure.tools.reader.reader-types :as rt]))
 
-(def LB :LB)
-(def RB :RB)
 
 (def KEYWORDS {"lambda" :lambda
                "def" :def
@@ -13,16 +11,14 @@
                "print" :print "read" :read
                })
 
-;;better tokens info - map or data type?
 ;;exception messages
-
 ;;exceptions on unknown characters
 ;;lines numbers - change parser to read line by line
 
 ;;test errors
 
-;;complete lexer overview
-;;master test - tokenize-test
+;;complete lexer overview;
+;master test - tokenize-test
 ;;terminating cases for all methods
 
 
@@ -122,3 +118,93 @@
       (if (nil? token)
         tokens
         (recur (conj tokens token) reader)))))
+
+;; :start starts with ""
+;; going to :start with something - put it, nil - don't put
+;; state transition - pass next, nothing - append character
+(defn append [sb char]
+  (.append sb char))
+(defn skip-char [sb char]
+  sb)
+(defn parse-integer [sb]
+  (-> sb .toString Integer/parseInt))
+(defn parse-double [sb]
+  (-> sb .toString Double/parseDouble))
+(defn keywordize [sb]
+  (-> sb .toString keyword))
+(defn skip-all [sb f] nil)
+(def grammar {
+              :start
+              {\( [:start LB]
+               \) [:start RB]
+               \" [:string skip-char]
+               whitespace? [:start skip-all]
+               \# [:boolean skip-char]
+               digit? [:integer append]
+               letter? [:name append]
+               }
+              
+              :string
+              {
+               letter? [:string append]
+               \" [:start skip-char #(.toString %)]
+               }
+              
+              :boolean
+              {\t [:start true]
+               \f [:start false]
+               }
+
+              :integer
+              {
+               digit? [:integer append]
+               \. [:double append]
+               (comp not digit?) [:start skip-char parse-integer]
+               }
+
+              :double
+              {
+               digit? [:double append]
+               (comp not digit?) [:start skip-char parse-double]
+               }
+
+              :name
+              {
+               letter? [:name append]
+               whitespace? [:start skip-char keywordize]
+               }
+              })
+
+
+(defn find-action [directions ch]
+  (directions (->> directions
+                   keys
+                   (filter #(or (= ch %) (and (fn? %) (% ch))))
+                   first
+                   )))
+  
+(defn modify [accum ch modifications]
+  (if (-> modifications first fn? not)
+    (first modifications)
+    ((apply comp (reverse modifications)) accum ch)))
+  
+(defn tokenize-with-grammar [grammar string]
+  (let [reader (rt/string-push-back-reader string)]
+    (loop [state :start accum nil reader reader tokens []]
+      (let [tokens (if (and (= state :start) (-> accum nil? not))
+                     (conj tokens accum) tokens)
+            accum (if (= state :start) (StringBuffer.) accum)
+            ch (rt/read-char reader)]
+        (if (and (= state :start) (nil? ch))
+          tokens
+          (let [directions (state grammar)
+                direction (find-action directions ch)
+                next-state (first direction)
+                next-accum (modify accum ch (rest direction))]
+            (recur next-state next-accum reader tokens)))))))
+      
+      
+
+;; start goes with empty string
+;; going back to start with anything other than nill adds it to output
+;; [a &b] - a is new state, b is either char or list of functions
