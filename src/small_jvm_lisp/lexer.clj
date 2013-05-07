eaa53efa8a4ad6165cfbaac00eae5b1231c0c2ee
Anton Logvinenko <logvinenko@farpost.com>
2013-05-07 17:19:35 +1100

    master Finishing work on lexer

--- src/small_jvm_lisp/lexer.clj
+++ src/small_jvm_lisp/lexer.clj
@@ -1,23 +1,17 @@
 (ns small-jvm-lisp.lexer
   (:require [clojure.tools.reader.reader-types :as rt]))
 
-(def KEYWORDS {"lambda" :lambda
-               "def" :def
-               "+" :plus "-" :minus
-               "/" :divide
-               "*" :multiply
-               "car" :car "cdr" :cdr "fn" :fn "cons" :cons "quote" :quote
-               "print" :print "read" :read
-               })
+(def KEYWORDS ["-" "*" "+" "=" "!=" "<" ">"
+               "def" "lambda"
+               "car" "cdr" "cons" "quote"])
 
-;;terminating cases for all methods + tests
 ;;automatic exceptions on unknown characters + tests
 ;;add line numbers at all exceptions
 ;;test errors
 
 ;;parsing keywords
-;;weird string characters
-
+;;add not equal
+;;terminating cases for all methods + tests
 ;;complete lexer overview;
 ;;master test - tokenize-test
 
@@ -31,9 +25,6 @@
 
 (defn digit? [ch]
   (and (-> ch nil? not) (Character/isDigit ch)))
-
-(defn word-symbol? [ch]
-  (or (letter? ch) (matches [\+ \- \/ \*] ch)))
 
 (defn whitespace? [ch]
   (or (nil? ch)  (Character/isWhitespace ch)))
@@ -54,34 +45,7 @@
 (defn return-char [sb reader char]
   (rt/unread reader char)
   sb)
-
-(def grammar {:done {\( [:done :LB]
-                     \) [:done :RB]
-                     \" [:string skip-char]
-                     whitespace? [:done skip-all]
-                     \# [:boolean skip-char]
-                     digit? [:integer append]
-                     word-symbol? [:name append]}
-              
-              :string {letter? [:string append]
-                       \" [:done skip-char #(.toString %)]
-                       [\tab \newline \return] [:string append]}
-              
-              :boolean {\t [:boolean true]
-                        \f [:boolean false]
-                        separator? [:done return-char]}
-
-              :integer {digit? [:integer append]
-                        \. [:double append]
-                        separator? [:done return-char parse-integer]}
-
-              :double {digit? [:double append]
-                       separator? [:done return-char parse-double]}
-              
-              :name {word-symbol? [:name append]
-                     separator? [:done return-char keywordize]}
-              })
-
+(def eof? nil?)
 (defn transition-ok? [ch t]
   (cond (char? t) (= t ch)
         (fn? t) (t ch)
@@ -118,6 +82,36 @@
               next-state (first transition)
               next-accum (modify accum reader ch (rest transition))]
           (recur next-state next-accum reader tokens))))))
+
+(def grammar {:done {\( [:done :LB]
+                     \) [:done :RB]
+                     \" [:string skip-char]
+                     whitespace? [:done skip-all]
+                     \# [:boolean skip-char]
+                     digit? [:integer append]
+                     letter? [:name append]
+                     [\+ \- \\ \* \= \< \>] [:keyword append]}
+              
+              :string {letter? [:string append]
+                       \" [:done skip-char #(.toString %)]
+                       [\tab \newline \return] [:string append]}
+              
+              :boolean {\t [:boolean true]
+                        \f [:boolean false]
+                        separator? [:done return-char]}
+
+              :integer {digit? [:integer append]
+                        \. [:double append]
+                        separator? [:done return-char parse-integer]}
+
+              :double {digit? [:double append]
+                       separator? [:done return-char parse-double]}
+              
+              :name {letter? [:name append]
+                     separator? [:done return-char keywordize]}
+
+              :keyword {separator? [:done return-char keywordize]}
+              })
 
 (defn tokenize [string]
   (->> string
