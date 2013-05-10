ff1e7424adecd26d7237271e4fe1dd75cda88a6e
Anton Logvinenko <logvinenko@farpost.com>
2013-05-10 13:51:50 +1100

    master Added errors messaging to syntax analyzer

--- src/small_jvm_lisp/syntax.clj
+++ src/small_jvm_lisp/syntax.clj
@@ -11,12 +11,24 @@
   (conj (pop stack)
         (conj (last stack) elem)))
 
+(defn raise-unmatched-brace [stack]
+  (->> (last stack)
+       (str "Unmatched brace, s-expression: ")
+       RuntimeException.
+       throw))
+
+(defn raise-unknown-token [token]
+  (->> token
+       (str "Unknown token")
+       RuntimeException.
+       throw))
+
 (defn read-sexpr [tokens]
   (loop [tokens tokens stack []]
     (let [token (first tokens)
           tokens (rest tokens)]
       (if (nil? token)
-        (throw (RuntimeException. "Ooops"))
+        (raise-unmatched-brace stack)
         (if (and (-> stack count (= 1)) (= token :RB))
           {:expr (first stack) :tokens tokens}
           (let [new-stack (case token
@@ -31,7 +43,7 @@
      (cond
       (= :LB token) (read-sexpr tokens)
       no-list? {:expr token :tokens (rest tokens)}
-      :else (throw (RuntimeException. "Ooops")))))
+      :else (raise-unknown-token token))))
 
 (defn read-program [tokens]
   (loop [expressions [] tokens tokens]
