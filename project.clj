c0b02f372b4deed31e7a773d004e2e0b64260065
Anton Logvinenko <logvinenko@farpost.com>
2013-05-30 14:00:10 +1100

    master Fixed Clojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject jacket "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.reader "0.7.4"]
                  ]
   :main jacket.core)
