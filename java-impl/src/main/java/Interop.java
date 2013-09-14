e44f488fbc84f5f57330feb7b92616527f25a17e
Anton Logvinenko <logvinenko@farpost.com>
2013-09-14 17:31:51 +1100

    jacket-32 Java objects fields and methods access

---
+++ java-impl/src/main/java/Interop.java
@@ -0,0 +1,176 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.lang.reflect.Method;
+
+final public class Interop {
+
+	private Interop() {
+	}
+
+	public static void main(String... args)
+		throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException,
+		InvocationTargetException, NoSuchFieldException {
+		System.out.println(accessInstance(new Long(42), "doubleValue", new Object[]{}));
+	}
+
+	private final static Map<Class, Class> typingMap = new HashMap<Class, Class>() {{
+		put(int.class, Long.class);
+		put(Integer.class, Long.class);
+		put(byte.class, Long.class);
+		put(Byte.class, Long.class);
+		put(short.class, Long.class);
+		put(Short.class, Long.class);
+		put(long.class, Long.class);
+		put(float.class, Double.class);
+		put(Float.class, Double.class);
+		put(double.class, Double.class);
+		put(boolean.class, Boolean.class);
+	}};
+
+	/**
+	 * Accessing
+	 */
+	public static Object accessInstance(Object object, String objectThing, Object[] arguments)
+		throws IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+		if (arguments.length > 0) {
+			return invokeMethod(object, objectThing, arguments);
+		}
+		try {
+			return getField(object, objectThing);
+		} catch (NoSuchFieldException e) {
+			return invokeMethod(object, objectThing, arguments);
+		}
+	}
+
+	private static Object getField(Object object, String fieldName)
+		throws NoSuchFieldException, IllegalAccessException {
+		return object.getClass().getField(fieldName).get(object);
+	}
+
+	private static Object invokeMethod(Object object, String methodName, Object[] arguments)
+		throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+		Method method = findMethod(object.getClass(), methodName, arguments);
+		if (method == null) {
+			throw new RuntimeException("No method '" + methodName + "' found");
+		}
+		Class<?>[] types = method.getParameterTypes();
+		arguments = castArguments(arguments, types);
+		return method.invoke(object, arguments);
+	}
+
+	/**
+	 * Instantiating
+	 */
+	public static Object instantiate(String className, Object[] arguments)
+		throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+		InstantiationException {
+		Constructor ctor = findConstructor(className, arguments);
+		if (ctor == null) {
+			throw new RuntimeException("Constructor not found");
+		}
+		Class<?>[] types = ctor.getParameterTypes();
+		arguments = castArguments(arguments, types);
+		return ctor.newInstance(arguments);
+	}
+
+	/**
+	 * Find utilities
+	 */
+	private static Constructor findConstructor(String className, Object[] arguments) throws ClassNotFoundException {
+		Constructor[] ctors = Class.forName(className).getDeclaredConstructors();
+		for (Constructor ctor : ctors) {
+			Class<?>[] ctorTypes = ctor.getParameterTypes();
+			if (signaturesMatch(arguments, ctorTypes)) {
+				return ctor;
+			}
+		}
+
+		return null;
+	}
+
+	static Method findMethod(Class<?> c, String methodName, Object[] arguments) throws ClassNotFoundException {
+		Method[] ctors = c.getDeclaredMethods();
+		for (Method method : ctors) {
+			if (method.getName().equals(methodName)) {
+				Class<?>[] ctorTypes = method.getParameterTypes();
+				if (signaturesMatch(arguments, ctorTypes)) {
+					return method;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Casting arguments on call
+	 */
+	static public Object[] castArguments(Object[] arguments, Class<?>[] types) {
+		for (int i = 0; i < arguments.length; i++) {
+			Object arg = arguments[i];
+			Class<?> type = types[i];
+			if ((arg instanceof Long && !type.equals(Long.class) && !type.equals(Integer.class)) ||
+				(arg instanceof Double && !type.equals(Double.class) && !type.equals(double.class))) {
+				arguments[i] = numberCast((Number) arg, type);
+			}
+		}
+		return arguments;
+	}
+
+	static public Object numberCast(Number arg, Class<?> type) {
+		if (type.equals(float.class) || type.equals(Float.class)) {
+			return arg.floatValue();
+		}
+		if (type.equals(int.class) || type.equals(Integer.class)) {
+			return arg.intValue();
+		}
+		if (type.equals(byte.class) || type.equals(Byte.class)) {
+			return arg.byteValue();
+		}
+		if (type.equals(short.class) || type.equals(Short.class)) {
+			return arg.shortValue();
+		}
+		return arg;
+	}
+
+	/**
+	 * Matching signatures
+	 */
+	private static boolean signaturesMatch(Object[] arguments, Class<?>[] ctorTypes) {
+		Class<?>[] argTypes = getTypes(arguments);
+
+		if (ctorTypes.length != argTypes.length) {
+			return false;
+		}
+		ctorTypes = convertParameterTypes(ctorTypes);
+		for (int i = 0; i < argTypes.length; i++) {
+			if (!ctorTypes[i].isAssignableFrom(argTypes[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static Class<?>[] getTypes(Object[] arguments) {
+		int argsLength = arguments.length;
+
+		Class<?>[] argTypes = new Class<?>[argsLength];
+		for (int i = 0; i < argsLength; i++) {
+			argTypes[i] = arguments[i].getClass();
+		}
+
+		return convertParameterTypes(argTypes);
+	}
+
+
+	private static Class<?>[] convertParameterTypes(Class<?>[] types) {
+		for (int i = 0; i < types.length; i++) {
+			Class<?> type = types[i];
+			if (typingMap.containsKey(type)) {
+				types[i] = typingMap.get(type);
+			}
+		}
+		return types;
+	}
+}
---
+++ java-impl/src/main/java/Numbers.java
@@ -0,0 +1,106 @@
+public class Numbers {
+
+	public static Double addD(Double a1, Double a2) {
+		return a1 + a2;
+	}
+
+	public static Long addL(Long a1, Long a2) {
+		return a1 + a2;
+	}
+
+	private static Number addN(Number a1, Number a2) {
+		if (a1 instanceof Double || a2 instanceof Double) {
+			return addD(a1.doubleValue(), a2.doubleValue());
+		}
+		if (a1 instanceof Long || a2 instanceof Long) {
+			return addL(a1.longValue(), a2.longValue());
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+	public static Object add(Object a1, Object a2) {
+		if (a1 instanceof Number && a2 instanceof Number) {
+			return addN((Number) a1, (Number) a2);
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+
+	private static Double mulD(Double a1, Double a2) {
+		return a1 * a2;
+	}
+
+	private static Long mulL(Long a1, Long a2) {
+		return a1 * a2;
+	}
+
+	private static Number mulN(Number a1, Number a2) {
+		if (a1 instanceof Double || a2 instanceof Double) {
+			return mulD(a1.doubleValue(), a2.doubleValue());
+		}
+		if (a1 instanceof Long || a2 instanceof Long) {
+			return mulL(a1.longValue(), a2.longValue());
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+	public static Object mul(Object a1, Object a2) {
+		if (a1 instanceof Number && a2 instanceof Number) {
+			return mulN((Number) a1, (Number) a2);
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+
+	private static Double subD(Double a1, Double a2) {
+		return a1 - a2;
+	}
+
+	private static Long subL(Long a1, Long a2) {
+		return a1 - a2;
+	}
+
+	private static Number subN(Number a1, Number a2) {
+		if (a1 instanceof Double || a2 instanceof Double) {
+			return subD(a1.doubleValue(), a2.doubleValue());
+		}
+		if (a1 instanceof Long || a2 instanceof Long) {
+			return subL(a1.longValue(), a2.longValue());
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+	public static Object sub(Object a1, Object a2) {
+		if (a1 instanceof Number && a2 instanceof Number) {
+			return subN((Number) a1, (Number) a2);
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+
+	private static Double divD(Double a1, Double a2) {
+		return a1 / a2;
+	}
+
+	private static Long divL(Long a1, Long a2) {
+		return a1 / a2;
+	}
+
+	private static Number divN(Number a1, Number a2) {
+		if (a1 instanceof Double || a2 instanceof Double) {
+			return divD(a1.doubleValue(), a2.doubleValue());
+		}
+		if (a1 instanceof Long || a2 instanceof Long) {
+			return divL(a1.longValue(), a2.longValue());
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+	public static Object div(Object a1, Object a2) {
+		if (a1 instanceof Number && a2 instanceof Number) {
+			return divN((Number) a1, (Number) a2);
+		}
+		throw new RuntimeException("Exceptional cake");
+	}
+
+}
